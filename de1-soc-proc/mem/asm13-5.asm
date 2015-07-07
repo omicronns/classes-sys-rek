6fa91a00f78d49e80643afbbdf8adc9b47a10752
omicronns <omicronns@gmail.com>
2015-07-07 20:31:05 +0200

    asms added

---
+++ de1-soc-proc/mem/asm13-5.asm
@@ -0,0 +1,8 @@
+movi	r0,	0
+movi	r1,	4
+addi	r0,	1
+mov	r2,	r0
+and	r2,	r1
+jzi	r2,	2
+movi	r3,	1
+ji	7
---
+++ de1-soc-proc/mem/asmdatatest.asm
@@ -0,0 +1,18 @@
+movi	r0,	0
+movi	r1,	20
+addi	r0,	1
+movdo	r0,	r0
+subi	r1,	1
+jnzi	r1,	2
+movi	r0,	0
+movi	r1,	20
+addi	r0,	1
+movdi	r2,	r0
+movso	gpo,	r2
+movi	r3,	10
+subi	r3,	1
+jnzi	r3,	12
+subi	r1,	1
+jnzi	r1,	8
+ji	0
+
